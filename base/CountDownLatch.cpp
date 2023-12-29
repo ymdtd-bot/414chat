c9459d99048a221cf2d8d3b4e1f69dabc293277b
weiyangyang <2746740439@qq.com>
2023-12-29 20:29:45 +0800

    zky first commit

---
+++ base/CountDownLatch.cpp
@@ -0,0 +1,31 @@
+﻿#include "CountDownLatch.h"
+
+CountDownLatch::CountDownLatch(int count)
+    : count_(count)
+{
+}
+
+void CountDownLatch::wait()
+{
+    std::unique_lock<std::mutex> lock(m_mutex);
+    while (count_ > 0)
+    {
+        condition_.wait(lock);
+    }
+}
+
+void CountDownLatch::countDown()
+{
+    std::unique_lock<std::mutex> lock(m_mutex);
+    --count_;
+    if (count_ == 0)
+    {
+        condition_.notify_all();
+    }
+}
+
+int CountDownLatch::getCount() const
+{
+    std::unique_lock<std::mutex> lock(m_mutex);
+    return count_;
+}
---
+++ base/CountDownLatch.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include <mutex>
+#include <condition_variable> 
+
+class CountDownLatch
+{
+public:
+
+    explicit CountDownLatch(int count);
+
+    void wait();
+
+    void countDown();
+
+    int getCount() const;
+
+private:
+    mutable std::mutex      m_mutex;
+    std::condition_variable condition_;
+    int                     count_;
+};
